7f6cc56918235c1c40b1f874fff6be0dedc33e3c
JP Silva <jsilva@thoughtworks.com>
2018-10-31 18:47:43 -0300

    Adds terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_file = "/Users/jsilva/.aws/credentials"
+  shared_credentials_file = "~/.aws/credentials"
   profile                 = "de_training"
 }
 
