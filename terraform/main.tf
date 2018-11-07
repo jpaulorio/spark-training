b84ae7f6cfb55efc2a8a6d7be62ed53f8303bdb3
JP Silva <jsilva@thoughtworks.com>
2018-11-07 13:15:00 -0300

    Changes main class

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,21 +35,21 @@ module "spark_facilitador" {
   emr_service_role_arn = "${module.iam.emr_service_role_arn}"
 }
 
-module "spark_par_01" {
-  source = "./spark"
-  project = "DE Training Recife"
-  environment = "Sandbox"
-  name = "par_01"
-  vpc_id = "${module.network.vpc_id}"
-  subnet_id = "${module.network.subnet_id}"
-  configurations = ""
-  key_name = "de-training-recife"
-  bootstrap_name = "null"
-  bootstrap_uri = "null"
-  log_uri = "s3://com.thoughtworks.training.de.recife/par_01/logs"
-
-  sg_emr_master_id = "${module.network.sg_emr_master_id}"
-  sg_emr_slave_id = "${module.network.sg_emr_slave_id}"
-  emr_ec2_instance_profile_arn = "${module.iam.emr_ec2_instance_profile_arn}"
-  emr_service_role_arn = "${module.iam.emr_service_role_arn}"
-}
+//module "spark_par_01" {
+//  source = "./spark"
+//  project = "DE Training Recife"
+//  environment = "Sandbox"
+//  name = "par_01"
+//  vpc_id = "${module.network.vpc_id}"
+//  subnet_id = "${module.network.subnet_id}"
+//  configurations = ""
+//  key_name = "de-training-recife"
+//  bootstrap_name = "null"
+//  bootstrap_uri = "null"
+//  log_uri = "s3://com.thoughtworks.training.de.recife/par_01/logs"
+//
+//  sg_emr_master_id = "${module.network.sg_emr_master_id}"
+//  sg_emr_slave_id = "${module.network.sg_emr_slave_id}"
+//  emr_ec2_instance_profile_arn = "${module.iam.emr_ec2_instance_profile_arn}"
+//  emr_service_role_arn = "${module.iam.emr_service_role_arn}"
+//}
